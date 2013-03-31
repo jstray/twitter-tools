c12e766e187461c6fb3f4024a7ded5d513a63c78
jstray <jonathanstray@gmail.com>
2013-03-31 00:59:34 -0400

    Initial version taking command line args

--- tweet-capture.py
+++ tweet-capture.py
@@ -3,6 +3,7 @@ import os
 import time
 import tweetstream
 import json
+import argparse
 
 def curDateFilename(count):
   now = datetime.datetime.now()
@@ -23,9 +24,22 @@ def logFile():
 def newDay(oldNow):
   now = datetime.datetime.now()
   return now.day != oldNow.day
-      
-words = ["gun violence","gun control","backround check","second amendment","2nd amendment","2nd amend","gun owner","gun owners","NRA","gun law","gun laws","run rights","gun crimes","firearm violence","gun makers","gun speech"]
-stream = tweetstream.FilterStream("overviewproject", "srslywtf?", track=words)
+
+# --- Main ---
+parser = argparse.ArgumentParser(description='Capture tweets containing keywords')
+parser.add_argument('-u', '--username', required=True, help='Twitter username')
+parser.add_argument('-p', '--password', required=True, help='Twitter password')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-f', '--termfile', help='file with terms to capture, one per line')
+group.add_argument('-t', '--terms',    help='terms to capture')
+args = parser.parse_args()
+
+if args.termfile:
+  words = [line.strip() for line in open(args.termfile)]
+else
+  words = [term.strip() for word in args.terms.split(',')]  
+
+stream = tweetstream.FilterStream(args.username, args.password, track=words)
 
 while True:
   logstart,f = logFile()
@@ -36,8 +50,11 @@ while True:
         f.close()
         break
   except tweetstream.ConnectionError, e:
-    print "Disconnected from twitter. Reason:", e.reason
-    time.sleep(15)
+    f = open('tweet-capture-error.log', 'a')
+    tm = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    f.write(tm + " -- " + e.reason +  "\n")
+    f.close
+    time.sleep(15) # chill for a moment, don't reconnect ceaselessly
 
 
     
